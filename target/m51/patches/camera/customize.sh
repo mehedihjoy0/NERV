17d793a3abb83b0a82e76bfc2363faa5433f86bd
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-08-05 12:52:23 +0600

    Update customize.sh

--- target/m51/patches/camera/customize.sh
+++ target/m51/patches/camera/customize.sh
@@ -1,6 +1,6 @@
 if ! grep -q "Camera End" "$WORK_DIR/vendor/ueventd.rc"; then
     echo "" >> "$WORK_DIR/vendor/ueventd.rc"
-    cat "$SRC_DIR/target/m52xq/patches/camera/ueventd" >> "$WORK_DIR/vendor/ueventd.rc"
+    cat "$SRC_DIR/target/m51/patches/camera/ueventd" >> "$WORK_DIR/vendor/ueventd.rc"
 fi
 
 BLOBS_LIST="
